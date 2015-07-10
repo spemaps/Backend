ae3cd2bbb175720fe281713b6880c9e720b7c191
angelawh <angelah@princeton.edu>
2015-07-10 15:47:37 -0400

    Added path function

--- cairo/graph_draw.py
+++ cairo/graph_draw.py
@@ -4,9 +4,7 @@ import json
 
 filename = raw_input('filename: ')
 graph = json.loads(open(filename).read())
-print graph
 image = graph.get('image')
-print image
 nodes = graph.get('nodes')
 edges = graph.get('edges')
 
@@ -34,6 +32,7 @@ def y_scale (y):
 def draw_node(cx,cy,r, rgb):
 	context.arc(x_scale(cx), y_scale(cy), r, 0, 2 * math.pi)
 	context.set_source_rgb(rgb[0]/255.0,rgb[1]/255.0,rgb[2]/255.0)
+	context.fill()
 	context.stroke()
 
 #function to draw edges
@@ -62,9 +61,10 @@ def draw_graph():
 		draw_node(node.get('coords')[0], node.get('coords')[1], .01, [0,0,0])
 
 def draw_path(path):
+	result = {"key": "value"}
 	for edge in path:
-		node_1 = edge.get('coords')[0]
-		node_2 = edge.get('coords')[1]
+		node_1 = edge[0]
+		node_2 = edge[1]
 		coords_1 = 0
 		coords_2 = 0
 		for node in nodes:
@@ -73,11 +73,24 @@ def draw_path(path):
 			if node.get('id') is node_2:
 				coords_2 = node.get('coords')
 		draw_edge(coords_1[0], coords_1[1], coords_2[0], coords_2[1], [255, 0, 0], .009)
-		draw_node(coords_1[0], coords_1[1])
-		draw_node(coords_2[0], coords_2[1])
+		#find start and end nodes
+		if not node_1 in result:
+			result[node_1] = 1 
+		else: result[node_1] += 1
+		if not node_2 in result:
+			result[node_2] = 1
+		else: result[node_2] += 1
+	endpoints = []
+	for key in result:
+		if result.get(key) is 1:
+			endpoints.append(key)
+	for node in nodes: #draw endpoints
+		if node['id'] is endpoints[0] or node['id'] is endpoints[1]:
+			draw_node(node.get('coords')[0], node.get('coords')[1], .01, [255,0,0])
 
 
 draw_graph()
+draw_path([[3,2],[4,3],[5,4],[5,7],[9,7],[10,9]])
 
 new_image = 'graph' + image
 surface.write_to_png (new_image) # Output to PNG
