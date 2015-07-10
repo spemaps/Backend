c4280583ea5339cf626d769a80b62e7cf60fd4a6
angelawh <angelah@princeton.edu>
2015-07-10 15:17:41 -0400

    iueiauodfiajsdlkf

--- cairo/graph_draw.py
+++ cairo/graph_draw.py
@@ -31,18 +31,19 @@ def y_scale (y):
 	height = 1000.0 * HEIGHT / WIDTH
 	return (y + .5) / height
 
-def draw_node(cx,cy,r):
+def draw_node(cx,cy,r, rgb):
 	context.arc(x_scale(cx), y_scale(cy), r, 0, 2 * math.pi)
-	context.set_source_rgb(0,0,0)
+	context.set_source_rgb(rgb[0]/255.0,rgb[1]/255.0,rgb[2]/255.0)
 	context.stroke()
 
 #function to draw edges
-def draw_edge(x0,y0,x1,y1):
+def draw_edge(x0,y0,x1,y1, rgb, width):
 	context.move_to(x_scale(x0),y_scale(y0))
 	context.line_to(x_scale(x1),y_scale(y1))
-	context.set_source_rgb(0,0,0)
-	context.set_line_width(0.005)
+	context.set_source_rgb(rgb[0]/255.0,rgb[1]/255.0,rgb[2]/255.0)
+	context.set_line_width(width)
 	context.stroke()
+
 
 def draw_graph():
 	for edge in edges:
@@ -55,10 +56,26 @@ def draw_graph():
 				coords_1 = node.get('coords')
 			if node.get('id') is node_2:
 				coords_2 = node.get('coords')
-		draw_edge(coords_1[0], coords_1[0], coords_2[0], coords_2[1])
+		draw_edge(coords_1[0], coords_1[1], coords_2[0], coords_2[1], [0,0,0], .005)
 
 	for node in nodes:
-		draw_node(node.get('coords')[0], node.get('coords')[1], .01)
+		draw_node(node.get('coords')[0], node.get('coords')[1], .01, [0,0,0])
+
+def draw_path(path):
+	for edge in path:
+		node_1 = edge.get('coords')[0]
+		node_2 = edge.get('coords')[1]
+		coords_1 = 0
+		coords_2 = 0
+		for node in nodes:
+			if node.get('id') is node_1:
+				coords_1 = node.get('coords')
+			if node.get('id') is node_2:
+				coords_2 = node.get('coords')
+		draw_edge(coords_1[0], coords_1[1], coords_2[0], coords_2[1], [255, 0, 0], .009)
+		draw_node(coords_1[0], coords_1[1])
+		draw_node(coords_2[0], coords_2[1])
+
 
 draw_graph()
 
